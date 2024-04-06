822db2eabde28037ea15491c38357bdbeb0edb08
aman <amansaxena8055@gmail.com>
2024-04-06 20:15:12 +0530

    add delete request scenario

--- Working-with-HTTP-Requests/projects/http-01-starting-setup/assets/scripts/app.js
+++ Working-with-HTTP-Requests/projects/http-01-starting-setup/assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 const form = document.querySelector("#new-post form");
 const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -38,6 +39,7 @@ async function fetchPosts() {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
     postEl.querySelector("p").textContent = post.body;
+    postEl.querySelector("li").id = post.id;
     listElement.append(postEl);
   }
 }
@@ -63,4 +65,14 @@ form.addEventListener("submit", (event) => {
   const enteredContent = event.currentTarget.querySelector("#content").value;
 
   createPosts(enteredTitle, enteredContent);
+});
+
+postList.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const postId = event.target.closest("li").id;
+    sendHttpRequest(
+      "DELETE",
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
 });
